e2192488a57bd0b1d94648ace251c25f989f646b
Sasha <sasha4474@ukr.net>
2021-03-19 16:42:01 +0200

    add site

---
+++ immovables/mark-up/js/main.js
@@ -0,0 +1,122 @@
+
+
+// Плавный переход по навигации
+
+$(function() {
+
+	$(window).scroll(function() {
+
+	if($(this).scrollTop() != 0) {
+
+	$('#toTop').fadeIn();
+
+	} else {
+
+	$('#toTop').fadeOut();
+
+	}
+
+	});
+
+	$('#toTop').click(function() {
+
+	$('body,html').animate({scrollTop:0},800);
+
+	});
+
+
+    $(window).scroll(function(){
+        if ( $(this).scrollTop() > 0 ) {
+            $('.main-header').addClass('header-scroll');
+        } else {
+            $('.main-header').removeClass('header-scroll');
+        }
+    })
+
+});
+
+
+
+// page init
+jQuery(function(){
+	jQuery('.burger').clickClass({
+		classAdd: 'menu-opened',
+		addToParent: true
+	});
+});
+
+/*
+ * Add class plugin
+ */
+jQuery.fn.clickClass = function(opt) {
+	var options = jQuery.extend({
+		classAdd: 'add-class',
+		addToParent: false,
+		event: 'click'
+	}, opt);
+
+	return this.each(function() {
+		var classItem = jQuery(this);
+		if(options.addToParent) {
+			if(typeof options.addToParent === 'boolean') {
+				classItem = classItem.parent();
+			} else {
+				classItem = classItem.parents('.' + options.addToParent);
+			}
+		}
+		jQuery(this).bind(options.event, function(e) {
+			classItem.toggleClass(options.classAdd);
+			e.preventDefault();
+			return false;
+		});
+	});
+};
+
+$(document).ready(function () {
+    $(".slider-1").slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+      ],
+    });
+  });
+
+  $(document).ready(function () {
+    $(".slider-2").slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+      ],
+    });
+  });
+
+
+
+
+
+
+
