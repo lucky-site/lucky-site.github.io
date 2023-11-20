35f07e7e2697c97df4e5e03055ee411690e1eaf1
sasha <sasha4474@ukr.net>
2023-11-20 13:08:11 +0200

    change portfolio

--- js/main.js
+++ js/main.js
@@ -1,42 +1,4 @@
-// var menu_selector = ".top-menu"; // Переменная должна содержать название класса или идентификатора, обертки нашего меню.
- 
-// function onScroll(){
-//     var scroll_top = $(document).scrollTop();
-//     $(menu_selector + " a").each(function(){
-//         var hash = $(this).attr("href");
-//         var target = $(hash);
-//         if (target.position().top <= scroll_top && target.position().top + target.outerHeight() > scroll_top) {
-//             $(menu_selector + " a.active").removeClass("active");
-//             $(this).addClass("active");
-//         } else {
-//             $(this).removeClass("active");
-//         }
-//     });
-// }
- 
-// $(document).ready(function () {
- 
-//     $(document).on("scroll", onScroll);
- 
-//     $("a[href^=#]").click(function(e){
-//         e.preventDefault();
- 
-//         $(document).off("scroll");
-//         $(menu_selector + " a.active").removeClass("active");
-//         $(this).addClass("active");
-//         var hash = $(this).attr("href");
-//         var target = $(hash);
- 
-//         $("html, body").animate({
-//             scrollTop: target.offset().top
-//         }, 500, function(){
-//             window.location.hash = hash;
-//             $(document).on("scroll", onScroll);
-//         });
- 
-//     });
- 
-// });
+
 
 // Плавный переход по навигации
 
@@ -64,66 +26,28 @@ $(function() {
 
 });
 
-     $(document).ready(function(){
-        $(".top-menu").on("click","a", function (event) {
-            event.preventDefault();
-            var id  = $(this).attr('href'),
-                top = $(id).offset().top;
-            $('body,html').animate({scrollTop: top}, 1000);
-        });
-    });
-    // $(document).ready(function(){
-    //     $(".top-menu-2").on("click","a", function (event) {
-    //         event.preventDefault();
-    //         var id  = $(this).attr('href'),
-    //             top = $(id).offset().top;
-    //         $('body,html').animate({scrollTop: top-0}, 1000);
-    //     });
-    // });
 
-
-
-// page init
-jQuery(function(){
-	jQuery('.burger').clickClass({
-		classAdd: 'menu-opened',
-		addToParent: true
+$(document).ready(function(){
+	$(".top-menu").on("click","a", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1000);
 	});
 });
 
-/*
- * Add class plugin
- */
-jQuery.fn.clickClass = function(opt) {
-	var options = jQuery.extend({
-		classAdd: 'add-class',
-		addToParent: false,
-		event: 'click'
-	}, opt);
 
-	return this.each(function() {
-		var classItem = jQuery(this);
-		if(options.addToParent) {
-			if(typeof options.addToParent === 'boolean') {
-				classItem = classItem.parent();
-			} else {
-				classItem = classItem.parents('.' + options.addToParent);
-			}
-		}
-		jQuery(this).bind(options.event, function(e) {
-			classItem.toggleClass(options.classAdd);
-			e.preventDefault();
-			return false;
-		});
-	});
-};
+let burger = document.querySelector('.burger');
+
+burger.addEventListener('click', function() {
+	this.parentElement.classList.toggle('menu-opened');
+});
+
+
+
 
 var modal = document.querySelector('#myModal');
-// var modal2 = document.getElementById('myModal2');
-// var modal3 = document.getElementById('myModal3');
 var btns = document.querySelectorAll(".myBtn");
-// var btn2 = document.getElementById("myBtn2");
-// var btn3 = document.getElementById("myBtn3");
 var span = document.querySelectorAll(".close");
 
 for (let btn of btns) {
@@ -136,18 +60,6 @@ for (let btn of span) {
         modal.style.display = 'none';
     })
 }
-// btn.onclick = function() {
-//     modal.style.display = "block";
-// }
-// btn2.onclick = function() {
-//     modal2.style.display = "block";
-// }
-// btn3.onclick = function() {
-//     modal.style.display = "block";
-// }
-// span.onclick = function() {
-//     modal.style.display = "none";
-// }
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
